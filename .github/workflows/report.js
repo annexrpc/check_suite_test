12faecec92540b1b4aad5f980faeebd0affa32d8
Cameron Bytheway <bytheway.cameron@gmail.com>
2022-02-16 12:52:13 -0700

    fix report.js

--- .github/workflows/report.js
+++ .github/workflows/report.js
@@ -1,14 +1,14 @@
-module.exports = async (args) => {
-  const action = actions[args.github.event.action];
-  if (action) await action(args)
-};
-
-const actions = { requested, completed };
-
 const requested = async ({ github, context, core }) => {
   console.log('workflow requested', github, context)
 };
 
 const completed = async ({ github, context, core }) => {
   console.log('workflow completed', github, context)
+};
+
+const actions = { requested, completed };
+
+module.exports = async (args) => {
+  const action = actions[args.github.event.action];
+  if (action) await action(args)
 };
