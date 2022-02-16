8f7ac2a319a1cd99998cb29cb81ab57892f6ddda
Cameron Bytheway <bytheway.cameron@gmail.com>
2022-02-16 12:57:05 -0700

    use context

--- .github/workflows/report.js
+++ .github/workflows/report.js
@@ -1,15 +1,14 @@
 const requested = async ({ github, context, core }) => {
-  console.log('workflow requested', github, context)
+  console.log('workflow requested', context.payload)
 };
 
 const completed = async ({ github, context, core }) => {
-  console.log('workflow completed', github, context)
+  console.log('workflow completed', context.payload)
 };
 
 const actions = { requested, completed };
 
 module.exports = async (args) => {
-  console.log(args);
-  const action = actions[args.github.event.action];
+  const action = actions[args.context.payload.action];
   if (action) await action(args)
 };
